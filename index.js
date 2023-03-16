0a05800b09ad5bf92780e1fb0c2db3096bd67d37
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-03-16 16:38:26 +0300

    Completed assignmet

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+const currentUser = "Alex Mbugua Kibe";
+
+const welcomeMessage = "Welcome to Flatbook, " + (currentUser) + "!"
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+const firstInitial = currentUser.slice(0, currentUser.indexOf(" "));
+
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = "Welcome, " + (firstInitial) + "!"
+
+
+
